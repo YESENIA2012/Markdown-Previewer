7855e794e7840ee95c9d3b8d972a02cb76f7e8a5
YESENIA2012 <gyesenia173@gmail.com>
2022-09-12 14:00:04 -0500

    comment was deleted

--- src/preview.js
+++ src/preview.js
@@ -1,6 +1,5 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-/* import rehypeHighlight from 'rehype-highlight'; */
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
